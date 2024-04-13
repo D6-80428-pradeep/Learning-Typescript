596b8adb302b40e22745ea4c9f05cf81949e953b
74626pradeep <125987234+74626pradeep@users.noreply.github.com>
2024-04-13 20:33:17 +0530

    using-ts.ts file added

--- using-ts.ts
+++ using-ts.ts
@@ -3,7 +3,6 @@ const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
 function add(num1:number, num2:number) {
- 
   return +num1 + +num2;
 }
 button.addEventListener("click", function() {
