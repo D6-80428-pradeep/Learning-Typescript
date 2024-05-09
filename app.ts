348f2ff7c6b15049c0d21f1a82a65113195c6736
74626pradeep <125987234+74626pradeep@users.noreply.github.com>
2024-05-09 22:37:05 +0530

    Arrays

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 const person: {
     name:string,
     age:number
+    per:[string,number]
 }={
     name: 'pradeep',
-    age:24
+    age:24,
+    per:['str', 2]
 }
 
 console.log(person.name);
--- basics.ts
+++ basics.ts
@@ -5,6 +5,8 @@ const input2 = document.getElementById("num2")! as HTMLInputElement;
 function add(num1:number, num2:number) {
   return +num1 + +num2;
 }
+
+
 button.addEventListener("click", function() {
   console.log(add(+input1.value, +input2.value));
 });
