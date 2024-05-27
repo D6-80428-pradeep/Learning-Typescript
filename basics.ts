9edddacbb08e8985814a18b614a6e0533c635f93
D6-80428-pradeep <pradeepsinghsatna1998@gmail.com>
2024-05-27 13:21:08 +0530

    changes done

--- basics.ts
+++ basics.ts
@@ -1,12 +1,19 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
-
-function add(num1:number, num2:number) {
-  return +num1 + +num2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error('Incorrect input!');
+  // }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
+let number1: number;
+number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+let resultPhrase = 'Result is: ';
 
-button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
-});
+add(number1, number2, printResult, resultPhrase);
